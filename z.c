c084a91c4b5efa10136cfa1df82b96690ac8aca8
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-03 16:58:36 +0530

    Create z.c

---
+++ z.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    int i=2;
+    while(i<=n)
+    {
+        if(i==n)
+        {
+            printf("YES");
+        }
+         i=i*2;
+    }
+   
+}
+getch();
+}
