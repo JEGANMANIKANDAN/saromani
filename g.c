3d28110fbe2130184502e6693017de9a96ef4e84
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-03 16:38:37 +0530

    Create g.c

---
+++ g.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i;
+clrscr();
+scanf("%d",a);
+for(i=1;i<10;i++)
+{
+    if(i==a)
+   {
+   printf("YES"); 
+    }
+
+  }
+}
+getch();
+)
