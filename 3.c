c906d0bc81c0cdf1a74baf2ca9f5379757ead655
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-15 16:38:53 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#Include<conio.h>
+
+int main(void) 
+{
+	int n;
+  clrscr();
+	scanf("%d",&n);
+	if(n%7==0)
+	{
+	printf("yes");
+	
+	}
+	else
+	{
+	printf("no");
+	}
+	return 0;
+}
+getch();
+}
