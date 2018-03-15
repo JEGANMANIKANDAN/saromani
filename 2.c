d69fa7f1ec29f5722f69f98f4f0b689ea9151fe1
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-15 16:35:23 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    
+    int i=2;
+    while(i<=a)
+    {
+        if(i==a)
+        {
+            printf("YES");
+        }
+         i=i*2;
+    }
+   
+}
+getch();
+}
