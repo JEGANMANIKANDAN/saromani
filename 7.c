21cbabd359dbc1d31234e5fc7c335f94b5315758
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-16 14:56:03 +0530

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a,n,i,m=0;
+    void main();
+    scanf("%d",&n);
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&a);
+         m=m+a;
+     }
+printf("%d",m);
+    
+}
+getch();
+}
