40a1482c8d724de3919c952764294aa388248556
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-16 14:39:04 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#Include<conio.h>
+
+int main(void) 
+{
+	int a;
+  clrscr();
+	scanf("%d",&a);
+	if(a%13==0)
+	{
+	printf("yes");
+	
+	}
+	else
+	{
+	printf("no");
+	}
+	return 0;
+}
+getch();
+}
