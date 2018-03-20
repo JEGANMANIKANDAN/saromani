66cb15689567eaba99c2f417a2b8192365f91709
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-20 16:36:52 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h.
+
+void main()
+{
+    float celcius;
+    float kelvin=0;
+    clrscr();
+    printf("Enter the celcius: \n");
+    scanf("%f",&celcius);
+    kelvin=celcius+273;
+    printf("The temp is %f",kelvin);
+}
+getch();
+}
