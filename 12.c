497bc6fc147b1e6e9f00815cdcb16e76c55c1104
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-20 15:54:34 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the a and b and c value ");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {
+        if(a<c)
+        {
+        printf("YES");
+        }
+    }
+    else
+    {
+        printf("NO");
+    }
+}
+getch();
+}
