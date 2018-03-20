456484533ecc1064192ea739a630011724007d6d
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-20 16:43:06 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int p,n,r;
+    int si;
+    clrscr();
+    printf("Enter the p and n and r value: \n");
+    scanf("%d%d%d",&p,&n,&r);
+    
+    si=(p*n*r)/100;
+    printf("The si value is %d",si);
+}
+getch();
+}
