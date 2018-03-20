98db563f0c17b19dd13fb6cd17412316564beeee
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-20 16:29:57 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int a,b,c,volume;
+clrscr();
+printf("ENter the a nad b and c value : \n");
+scanf("%d%d%d",&a,&b,&c);
+volume=a*b*c;
+printf("The volume is %d",volume);
+}
+getch();
+}
